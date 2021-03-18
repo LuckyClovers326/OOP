a0aec829108dcf0f8c9d25767f954d163651dc6b
LuckyClovers326 <80062437+LuckyClovers326@users.noreply.github.com>
2021-03-18 12:15:19 -0400

    Add files via upload

---
+++ ClassExamples/src/com/asc/oop/main/RandomProgramApp.java
@@ -0,0 +1,21 @@
+package com.asc.oop.main;
+
+import java.util.Scanner;
+
+public class RandomProgramApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Program \n Please enter your name in the following format (First Last)");
+		String inpt 	=	 "";
+		String fName 	=	 "";
+		String lName 	=	 "";
+		
+		Scanner sc = new Scanner(System.in);
+		inpt = sc.nextLine();
+		// Chris Pierce
+		fName = inpt.substring();
+
+	}
+
+}
